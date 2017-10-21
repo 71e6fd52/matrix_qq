20236168f30289ea033b9bce21251e7a0a1e2f5a
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-10-21 19:37:31 +0800

    send text

--- lib/matrix_qq/matrix/send.rb
+++ lib/matrix_qq/matrix/send.rb
@@ -2,9 +2,13 @@ module MatrixQQ
   class Matrix
     # send message
     module Send
-      def self.send(dbus, room_id, event_type, body)
+      def self.raw(dbus, room_id, event_type, body)
         txn_id = SecureRandom.hex(32)
         dbus.put "/rooms/#{room_id}/send/#{event_type}/#{txn_id}", body
+      end
+
+      def self.text(dbus, room_id, message)
+        raw dbus, room_id, 'm.room.message', msgtype: 'm.text', body: message
       end
     end # Send
   end # Matrix
