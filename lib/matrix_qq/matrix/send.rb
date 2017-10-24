8698c02f91b554aa9f8f11530516484a6ecfb02d
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-10-24 19:19:26 +0800

    remember send event

--- lib/matrix_qq/matrix/send.rb
+++ lib/matrix_qq/matrix/send.rb
@@ -1,9 +1,15 @@
 module MatrixQQ
   class Matrix
     module Send
+      class << self
+        attr_accessor :ignore
+      end
+      self.ignore = []
+
       def self.raw(dbus, room_id, event_type, body)
         txn_id = SecureRandom.hex(32)
-        dbus.put "/rooms/#{room_id}/send/#{event_type}/#{txn_id}", body
+        res = dbus.put "/rooms/#{room_id}/send/#{event_type}/#{txn_id}", body
+        ignore << JSON.parse(res)['event_id']
       end
 
       def self.text(dbus, room_id, message)
