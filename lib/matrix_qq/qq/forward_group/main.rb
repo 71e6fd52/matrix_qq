cd3b5e0299ff326f9d8e4c1f637f8fe8e40a1ec2
71e6fd52 <DAStudio.71e6fd52@gmail.com>
2017-11-04 16:36:32 +0800

    group forword support all

--- lib/matrix_qq/qq/forward_group/main.rb
+++ lib/matrix_qq/qq/forward_group/main.rb
@@ -15,18 +15,34 @@ module MatrixQQ
 
       def run
         return unless @info.is_a? Hash
+        all = run_exact room, value
+        run_all room, value if all
+      end
+
+      def run_exact
         tunnel = Config[:tunnel][@info['group_id'].to_s]
-        return if tunnel.nil?
-        return unless tunnel[:type] == 'group'
+        return false if tunnel.nil?
+        return false unless tunnel[:type] == 'group'
         tunnel[:to].each_pair do |room, type|
-          call_module(room, type)
+          call_module room, type
+        end
+        MatrixQQ.intercept? tunnel
+      end
+
+      def run_all
+        tunnel = Config[:tunnel]['group']
+        return if tunnel.nil?
+        return unless tunnel[:type] == 'all'
+        tunnel[:to].each_pair do |room, type|
+          call_module room, type, print_room: true
         end
       end
 
-      def call_module(room, type)
+      def call_module(room, type, hackin = {})
+        info = @info.merge hackin
         ForwardGroup.send_to[type.to_s].each do |func|
           puts "Start #{func.name}" if $VERBOSE
-          func.new(@dbus, @matrix, @info, room).run
+          func.new(@dbus, @matrix, info, room).run
           puts "End #{func.name}" if $VERBOSE
         end
       end
